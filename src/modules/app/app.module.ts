52791e93ebb1a1ac5b43a9ecd58f20c1d3c185df
goodnesslight <evgenijmajboroda1@gmail.com>
2025-06-08 22:24:56 +0300

    Update: Installed and configured mongoose

--- src/modules/app/app.module.ts
+++ src/modules/app/app.module.ts
@@ -1,8 +1,19 @@
 import { DetectionModule } from '@modules/detection/detection.module';
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [ConfigModule.forRoot(), DetectionModule],
+  imports: [
+    ConfigModule.forRoot(),
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (service: ConfigService) => ({
+        uri: service.get<string>('MONGO_URI'),
+      }),
+    }),
+    DetectionModule,
+  ],
 })
 export class AppModule {}
