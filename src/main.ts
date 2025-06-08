06ffa01da16fe50da5bd4e35a6e0fd51ea22e092
goodnesslight <evgenijmajboroda1@gmail.com>
2025-06-08 23:49:12 +0300

    Update: Getting SERVER_PORT from .env

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,12 @@
 import { INestApplication } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from 'src/modules/app/app.module';
 
 async function bootstrap(): Promise<void> {
   const app: INestApplication = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const config: ConfigService = app.get(ConfigService);
+  await app.listen(config.get<number>('SERVER_PORT'));
 }
 
 bootstrap();
