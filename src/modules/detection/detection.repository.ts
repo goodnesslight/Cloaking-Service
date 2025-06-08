1b5783dfb27b20ab58018d7b15d5d1ef76f21dd5
goodnesslight <evgenijmajboroda1@gmail.com>
2025-06-08 23:55:03 +0300

    Update: Removed unnecessary default modifier

--- src/modules/detection/detection.repository.ts
+++ src/modules/detection/detection.repository.ts
@@ -1,4 +1,4 @@
 import { AbstractRepository } from 'src/common/abstracts/repository.abstracts';
 import { DetectionEntity } from './detection.entity';
 
-export default class DetectionRepository extends AbstractRepository<DetectionEntity> {}
+export class DetectionRepository extends AbstractRepository<DetectionEntity> {}
