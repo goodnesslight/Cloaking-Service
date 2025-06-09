eb8f4f7919154d7302e3da49ede87a5e37b130c1
goodnesslight <evgenijmajboroda1@gmail.com>
2025-06-09 17:13:17 +0300

    Update: Added @Injectable decorator

--- src/modules/detection/detection.repository.ts
+++ src/modules/detection/detection.repository.ts
@@ -1,4 +1,6 @@
 import { AbstractRepository } from '@common/abstracts/repository.abstract';
 import { DetectionEntity } from './detection.entity';
+import { Injectable } from '@nestjs/common';
 
+@Injectable()
 export class DetectionRepository extends AbstractRepository<DetectionEntity> {}
