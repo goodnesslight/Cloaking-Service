32d9c5be564b88b043ed28a954e6c9d70e22fb50
goodnesslight <evgenijmajboroda1@gmail.com>
2025-06-09 17:21:39 +0300

    Feat: Added validation pipes

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,4 @@
-import { INestApplication } from '@nestjs/common';
+import { INestApplication, ValidationPipe } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from '@modules/app/app.module';
@@ -6,6 +6,13 @@ import { AppModule } from '@modules/app/app.module';
 async function bootstrap(): Promise<void> {
   const app: INestApplication = await NestFactory.create(AppModule);
   const config: ConfigService = app.get<ConfigService>(ConfigService);
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+    }),
+  );
   await app.listen(config.get<number>('SERVER_PORT'));
 }
 
