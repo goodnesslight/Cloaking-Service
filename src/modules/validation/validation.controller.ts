efc6f4fd7e7462820b1ae3d7a152e319d75ae764
goodnesslight <evgenijmajboroda1@gmail.com>
2025-06-09 18:00:39 +0300

    Update: Added supporting of swagger docs for ValidationController

--- src/modules/validation/validation.controller.ts
+++ src/modules/validation/validation.controller.ts
@@ -1,13 +1,24 @@
+import { Controller, Post, Body } from '@nestjs/common';
+import { ApiTags, ApiOperation, ApiBody, ApiOkResponse } from '@nestjs/swagger';
 import { ValidationValidateDto } from '@modules/validation/api/dtos/validation-validate.dto';
 import { ValidationValidateResponse } from '@modules/validation/api/responses/validation-validate.response';
 import { ValidationService } from '@modules/validation/validation.service';
-import { Body, Controller, Post } from '@nestjs/common';
 
+@ApiTags('validation')
 @Controller('validation')
 export class ValidationController {
   constructor(private readonly service: ValidationService) {}
 
   @Post('validate')
+  @ApiOperation({
+    summary: 'Validates IP and detect bot usage',
+  })
+  @ApiBody({
+    type: ValidationValidateDto,
+  })
+  @ApiOkResponse({
+    type: ValidationValidateResponse,
+  })
   async validate(
     @Body() dto: ValidationValidateDto,
   ): Promise<ValidationValidateResponse> {
