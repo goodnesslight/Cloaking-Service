fd9cdeffbe448d8b3bd2a877d0eaaf20acf83b91
goodnesslight <evgenijmajboroda1@gmail.com>
2025-06-09 15:20:37 +0300

    Update: Namings, imports

--- src/modules/detection/detection.repository.ts
+++ src/modules/detection/detection.repository.ts
@@ -1,4 +1,4 @@
-import { AbstractRepository } from '@common/abstracts/repository.abstracts';
+import { AbstractRepository } from '@common/abstracts/repository.abstract';
 import { DetectionEntity } from './detection.entity';
 
 export class DetectionRepository extends AbstractRepository<DetectionEntity> {}
