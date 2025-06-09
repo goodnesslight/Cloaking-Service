d56bb7d027d31f6e7c27753a047ec3214a72f553
goodnesslight <evgenijmajboroda1@gmail.com>
2025-06-09 17:55:13 +0300

    Update: Added SwaggerModule

--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,21 @@ import { INestApplication, ValidationPipe } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from '@modules/app/app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap(): Promise<void> {
   const app: INestApplication = await NestFactory.create(AppModule);
   const config: ConfigService = app.get<ConfigService>(ConfigService);
+  const document = SwaggerModule.createDocument(
+    app,
+    new DocumentBuilder()
+      .setTitle('Cloaking-Service')
+      .setVersion('1.0')
+      .build(),
+  );
+
+  SwaggerModule.setup('api', app, document);
+
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
@@ -13,6 +24,7 @@ async function bootstrap(): Promise<void> {
       transform: true,
     }),
   );
+
   await app.listen(config.get<number>('SERVER_PORT'));
 }
 
