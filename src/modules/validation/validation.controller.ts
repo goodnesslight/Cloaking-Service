0a6d8697fd49cae4d00144758efba95ffb51a6fe
goodnesslight <evgenijmajboroda1@gmail.com>
2025-06-09 17:20:51 +0300

    Feat: Added route for validation

--- src/modules/validation/validation.controller.ts
+++ src/modules/validation/validation.controller.ts
@@ -1,7 +1,16 @@
+import { ValidationValidateDto } from '@modules/validation/api/dtos/validation-validate.dto';
+import { ValidationValidateResponse } from '@modules/validation/api/responses/validation-validate.response';
 import { ValidationService } from '@modules/validation/validation.service';
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
 
 @Controller('validation')
 export class ValidationController {
   constructor(private readonly service: ValidationService) {}
+
+  @Post('validate')
+  async validate(
+    @Body() dto: ValidationValidateDto,
+  ): Promise<ValidationValidateResponse> {
+    return await this.service.validate(dto);
+  }
 }
